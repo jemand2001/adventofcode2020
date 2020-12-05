8a0b820114f2f4b937b2a79932358ad04817bfd7
jemand2001 <>
2020-12-05 12:27:05 +0100

    day 5 golf

--- day_5.py
+++ day_5.py
@@ -1,4 +1,4 @@
-from utils import run, part
+from utils import run, part, ident
 from typing import List
 
 def coordinates(row: str, col: str):
@@ -19,7 +19,18 @@ def day_5_2(pids: List[int]):
     pids = set(pids)
     return [i + 1 for i in pids if i + 1 not in pids and i + 2 in pids][0]
 
+@run(ident)
+def day_5_1_golf(passes: List[str]):
+    return max(int(p.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), base=2) for p in passes)
+
+@run(ident)
+def day_5_2_golf(passes: List[str]):
+    ids = {int(p.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), base=2) for p in passes}
+    return [i + 1 for i in ids if i + 1 not in ids and i + 2 in ids][0]
+
 
 if __name__ == '__main__':
     print('highest ID:', day_5_1())
+    print('highest ID:', day_5_1_golf())
     print('your ID is:', day_5_2())
+    print('your ID is:', day_5_2_golf())
