d360687bb0398251074fbb40f800813460e76d90
jemand2001 <>
2020-12-06 18:45:34 +0100

    str.split instead of ident -> group.split()

--- day_6.py
+++ day_6.py
@@ -1,14 +1,14 @@
 from utils import run, ident
 from typing import List
 
-@run(ident, '\n\n')
+@run(str.split, '\n\n')
 def day_6_1(groups: List[str]):
-    return sum(len(set.union(*({*line} for line in group.split()))) for group in groups)
+    return sum(len(set.union(*({*line} for line in group))) for group in groups)
 
 
-@run(ident, '\n\n')
+@run(str.split, '\n\n')
 def day_6_2(groups: List[str]):
-    return sum(len(set.intersection(*({*line} for line in group.split()))) for group in groups)
+    return sum(len(set.intersection(*({*line} for line in group))) for group in groups)
 
 
 if __name__ == '__main__':
