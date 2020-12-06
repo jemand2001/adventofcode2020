6378b48f59416d30b034909d5d63ca2bbadec70a
jemand2001 <>
2020-12-06 13:26:15 +0100

    changed day 6 solutions from lists to generators

--- day_6.py
+++ day_6.py
@@ -3,12 +3,12 @@ from typing import List
 
 @run(ident, '\n\n')
 def day_6_1(groups: List[str]):
-    return sum(len(set.union(*[{*line} for line in group.split()])) for group in groups)
+    return sum(len(set.union(*({*line} for line in group.split()))) for group in groups)
 
 
 @run(ident, '\n\n')
 def day_6_2(groups: List[str]):
-    return sum(len(set.intersection(*[{*line} for line in group.split()])) for group in groups)
+    return sum(len(set.intersection(*({*line} for line in group.split()))) for group in groups)
 
 
 if __name__ == '__main__':
