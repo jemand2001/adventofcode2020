a1f9c687243601448ddf9231134b9ebc311e23af
jemand2001 <>
2020-12-09 11:09:44 +0100

    day 9 part 1 golf

--- day_9.py
+++ day_9.py
@@ -18,7 +18,12 @@ def day_9_2(numbers: List[int]):
             if sum(consideration) == 133015568:
                 return i, i + length + 1, max(consideration) + min(consideration)
 
+@run()
+def day_9_1_golf(nums):
+    return [n for i, n in enumerate(nums[25:], start=25) if not any(n - k in nums[i-25:i] for k in nums[i-25:i])][0]
+
 
 if __name__ == '__main__':
     print("first invalid:", day_9_1())
-    print("weakness:     ", day_9_2())
+    print("first invalid:", day_9_1_golf())
+    # print("weakness:     ", day_9_2())
