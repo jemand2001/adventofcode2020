fefe5c30f7210cee0cc9fb0412cbad8e3c304ade
jemand2001 <>
2020-12-18 00:49:24 +0100

    cleanup

--- day_17_1.py
+++ day_17_1.py
@@ -1,4 +1,4 @@
-from utils import run, test
+from utils import run
 from typing import List, Set, Iterator
 from collections import namedtuple
 
@@ -12,21 +12,16 @@ Dimension = Set[Point]
 
 
 def all_points(start: Point, end: Point) -> Iterator[Point]:
-    # print(f'yielding from {start} to {end}')
     for z in range(start.z, end.z + 1):
         for y in range(start.y, end.y + 1):
             for x in range(start.x, end.x + 1):
-                # print(n := Point(x, y, z))
                 yield Point(x, y, z)
 
 def neighbors(p: Point) -> Iterator[Point]:
-    # print(f'neighbors for {p}')
-    yield from (points := [*filter(lambda x: x != p, all_points(
+    yield from filter(lambda x: x != p, all_points(
         Point(p.x - 1, p.y - 1, p.z - 1),
         Point(p.x + 1, p.y + 1, p.z + 1)
-    ))])
-    # for point in points:
-    #     print(point)
+    ))
 
 def minimum(d: Dimension) -> Point:
     return Point(
@@ -45,35 +40,26 @@ def maximum(d: Dimension) -> Point:
 def cycle(previous: Dimension) -> Dimension:
     active: Dimension = set()
     for point in all_points(minimum(previous), maximum(previous)):
-        # print(f'processing: {point}')
         if point in previous and sum(p in previous for p in neighbors(point)) in (2, 3):
             active.add(point)
         elif point not in previous and sum(p in previous for p in neighbors(point)) == 3:
             active.add(point)
-    # print(active)
     return active
 
 
 @run(list)
-# @test(list, examples=examples)
 def day_17_1(start: List[List[str]]):
     active: Dimension = set()
     for y, row in enumerate(start):
         for x, _ in enumerate(row):
             if start[y][x] == '#':
                 active.add(Point(x, y, 0))
-    # prev = active
     print('cycle: 0')
     show_cubes(active)
     for i in range(1, 7):
-        try:
-            active = cycle(active)
-            print(f'cycle: {i}')
-            show_cubes(active)
-        except ValueError:
-            print(f'cycle {i}: {prev}')
-            exit(1)
-        prev = active
+        active = cycle(active)
+        print(f'cycle: {i}')
+        show_cubes(active)
     return len(active)
 
 def show_cubes(d: Dimension):
--- day_17_2.py
+++ day_17_2.py
@@ -1,4 +1,4 @@
-from utils import run, test
+from utils import run
 from typing import List, Set, Iterator
 from collections import namedtuple
 
@@ -12,22 +12,17 @@ Dimension = Set[Point]
 
 
 def all_points(start: Point, end: Point) -> Iterator[Point]:
-    # print(f'yielding from {start} to {end}')
     for w in range(start.w, end.w + 1):
         for z in range(start.z, end.z + 1):
             for y in range(start.y, end.y + 1):
                 for x in range(start.x, end.x + 1):
-                    # print(n := Point(x, y, z))
                     yield Point(x, y, z, w)
 
 def neighbors(p: Point) -> Iterator[Point]:
-    # print(f'neighbors for {p}')
-    yield from (points := [*filter(lambda x: x != p, all_points(
+    yield from filter(lambda x: x != p, all_points(
         Point(p.x - 1, p.y - 1, p.z - 1, p.w - 1),
         Point(p.x + 1, p.y + 1, p.z + 1, p.w + 1)
-    ))])
-    # for point in points:
-    #     print(point)
+    ))
 
 def minimum(d: Dimension) -> Point:
     return Point(
@@ -48,12 +43,10 @@ def maximum(d: Dimension) -> Point:
 def cycle(previous: Dimension) -> Dimension:
     active: Dimension = set()
     for point in all_points(minimum(previous), maximum(previous)):
-        # print(f'processing: {point}')
         if point in previous and sum(p in previous for p in neighbors(point)) in (2, 3):
             active.add(point)
         elif point not in previous and sum(p in previous for p in neighbors(point)) == 3:
             active.add(point)
-    # print(active)
     return active
 
 @run(list)
@@ -63,17 +56,10 @@ def day_17_2(start: List[List[str]]):
         for x, _ in enumerate(row):
             if start[y][x] == '#':
                 active.add(Point(x, y, 0, 0))
-    # prev = active
-    # print('cycle: 0')
-    # show_cubes(active)
     for i in range(1, 7):
         active = cycle(active)
-        # print(f'cycle: {i}')
-        # show_cubes(active)
-        prev = active
     return len(active)
 
 
 if __name__ == '__main__':
     print('active after 6 cycles:', day_17_2())
-
