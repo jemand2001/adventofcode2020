e210d1553703d14b627a30a81130955ba60954e0
jemand2001 <>
2020-12-18 22:27:37 +0100

    day 18 part 2 + some minor stuff

--- day_18.py
+++ day_18.py
@@ -1,6 +1,7 @@
 from utils import run, ident
 from typing import List, Tuple
 from operator import add, mul
+import re
 
 examples = (
     '''1 + 2 * 3 + 4 * 5 + 6''',
@@ -13,19 +14,18 @@ examples = (
 
 operators = {'*': mul, '+': add}
 
-def parse_expression_equal(line: str) -> Tuple[int, int]:
+def parse_equal(line: str) -> Tuple[int, int]:
     res = 0
     next_op = add
     i = 0
     while i < len(line):
         c = line[i]
-        if ord('0') <= ord(c) <= ord('9'):
+        if c.isdigit():
             res = next_op(res, int(c))
         elif c in operators:
             next_op = operators[c]
         elif c == '(':
-            print('descending!')
-            k, skipped = parse_expression_equal(line[i + 1:])
+            k, skipped = parse_equal(line[i + 1:])
             res = next_op(res, k)
             i += skipped + 1
         elif c == ')':
@@ -35,11 +35,21 @@ def parse_expression_equal(line: str) -> Tuple[int, int]:
         i += 1
     return res, len(line)
 
+def parse_pluses(line: str) -> int:
+    while match := re.fullmatch(r'(?P<prefix>.*?)\((?P<parens>[^()]+)\)(?P<suffix>.*)', line):
+        line = match['prefix'] + str(parse_pluses(match['parens'])) + match['suffix']
+    line = re.sub(r'(\d+(?: \+ \d+)+)', lambda m: str(eval(m[0])), line)
+    return eval(line)
+
 @run(ident)
 def day_18_1(lines: List[str]):
-    return sum(parse_expression_equal(line.replace(' ', ''))[0] for line in lines)
+    return sum(parse_equal(line.replace(' ', ''))[0] for line in lines)
 
+@run(ident)
+def day_18_2(lines: List[str]):
+    return sum(parse_pluses(line) for line in lines)
 
 
 if __name__ == '__main__':
     print('sum of all expressions (equal):', day_18_1())
+    print('sum of all expressions (pluses):', day_18_2())
